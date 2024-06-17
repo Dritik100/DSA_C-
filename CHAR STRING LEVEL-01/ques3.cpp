82a8bfa06e82126fedbe84e449556f1d0ddddb6d
Dritik100 <ritikdubey2664@gmail.com>
2024-06-17 16:45:20 +0530

    second commit

---
+++ CHAR STRING LEVEL-01/ques3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int findLength(char ch[], int size){
+    int length=0;
+    for(int i=0; i<size;i++){
+        if(ch[i]=='\0'){
+            break;
+        }
+        else{
+            length++;
+        }
+    }
+    return length;
+}
+ void reverseString(char ch[], int n){
+    int i=0;
+    int j=n-1;
+    while(i<=j){
+        swap(ch[i], ch[j]);
+        i++;
+        j--;
+    }
+ }
+int main(){
+   char ch[100];
+   cin.getline(ch,100);
+   cout<<"Before:"<<ch<<endl;
+   int len=findLength(ch,100);
+//    int len=findLength(ch,100);
+    reverseString(ch,len);
+    cout<<endl<<"After:"<<ch<<endl;
+}
