8bb4b821a456dfad86aeabfd2e5065cf46c99816
Dritik100 <ritikdubey2664@gmail.com>
2024-07-03 15:18:51 +0530

    second.commit

--- pattern/solidhalfdiamond.cpp
+++ pattern/solidhalfdiamond.cpp
@@ -7,9 +7,14 @@ int main(){
     for(int k=0;k<i;k++){
         
             cout<<"* ";
-                }
-               
         
+    }
             cout<<endl;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
     }
 }
