8fc1defb0f37c3165999b2b07e0813965cef05c7
Dritik100 <ritikdubey2664@gmail.com>
2024-07-11 13:33:37 +0530

    second commit

---
+++ Bitoperatorandloo/practice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+    // int a=7;
+    // int b=8;
+    // // cout<<(a&b)<<endl;
+    // // cout<<(a|b)<<endl;
+    // int num=33;
+    // cout<< ~num<<endl;
+    int a=7;
+    int b=8;
+    cout<<(a^b)<<endl;
+    return 0;
+ }
---
+++ Recursion/ques4fibonnaci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int fib(int n){
+    //base case
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    //recursive call
+    int ans= fib(n-1)+fib(n-2);
+    return ans;
+}
+int main(){
+    cout<<fib(8);
+    return 0;
+}
