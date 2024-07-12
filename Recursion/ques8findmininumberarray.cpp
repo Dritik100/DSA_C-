39dd74add493ceda9cd4be44ed8494164240127a
Dritik100 <ritikdubey2664@gmail.com>
2024-07-12 15:18:22 +0530

    second.commit

---
+++ Recursion/ques8findmininumberarray.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+void findMin(int arr[], int size, int index, int &mini){
+    //base case
+    if(index >= size){
+        return ;
+    }
+    //processing 
+    mini=min(mini, arr[index]);
+    //recursive call
+    findMin(arr,size, index+1, mini);
+}
+int main(){
+    int arr[]= {10,20, 30, 40, 50};
+    int size=5;
+    int index=0;
+    int mini = INT_MAX;
+    findMin(arr,size, index,mini);
+    cout<<"Minimum Number in array:"<<mini<<endl;
+    return 0;
+}
---
+++ Recursion/ques9findevennumberinvector.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<limits.h>
+#include<vector>
+using namespace std;
+void solve(int arr[], int size, int index, vector<int> &ans){
+    //base case
+    if(index>=size){
+        return;
+    }
+    //processing
+    if(arr[index]%2==0){
+        ans.push_back(arr[index]);
+    }
+    // Recursion Relation
+
+    solve(arr, size,index+1, ans);
+}
+int main(){
+    int arr[]= {11,12,13,14,15};
+    int size=5;
+    int index=0;
+    vector<int>ans;
+    solve(arr,size,index,ans);
+
+    //printing vector
+    // for(int nums:ans){
+    //     cout<<nums<<" "; 
+    // }
+
+    // for(int i=0; i<ans.size(); i++){
+    //     cout<<ans[i]<<" ";
+    // }
+
+    for(auto i: ans){
+        cout<<i <<" ";
+    }
+    return 0;
+}
