af81f287bcfbe2adbad2f94b2a376482a920033d
Ritk Dubey <ritikdubey2664@gmail.com>
2024-10-28 10:26:01 +0530

    factorail problem

--- pointerSecond/2video.cpp
+++ pointerSecond/2video.cpp
@@ -1,27 +1,31 @@
 #include<iostream>
 using namespace std;
-void solve(int *arr, int size){
-  //  cout<<"size of array inside function: "<<sizeof(arr)<<endl;
+int factorial(int n){
 
-  //  cout<<"inside solve -> arr:"<<arr<<endl;
-
-  //   cout<<"inside solve -> arr:"<<&arr<<endl;
-
-  *arr=*arr+1;
-
-   
-}
-
-int main(){
-  int arr[]={10,20,30, 40,50};
-  // cout<<"size of array: "<<sizeof(arr)<<endl;
-  solve(arr,5);
-
-  for(int i=0;i<5 ;i++){
-    cout<<arr[i]<<endl;
+  //base case
+  if(n==1){
+    return 1;
   }
+  if(n==0){
+    return 1;
+  }
+  
 
-  // cout<<"inside main -> arr:"<<arr<<endl;
-  // cout<<"inside main -> arr:"<<&arr<<endl;
+  //recursive realtion
+   int recursiveKaAns= factorial(n-1);
+
+   //processing
+   int finalAns= n * recursiveKaAns;
+   return finalAns;
+   
+
+}
+int main(){
+  int n;
+  cout<<"Enter the number: ";
+  cin>>n;
+
+
+ cout<<factorial(n)<<endl;
   return 0;
 }
