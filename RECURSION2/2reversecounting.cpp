b4181e8b404132ee2a7e993f2602d35a488b4c14
Ritk Dubey <ritikdubey2664@gmail.com>
2024-10-28 10:48:15 +0530

    Reverse countoing

---
+++ RECURSION2/2reversecounting.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void counting(int n) {
+  // Base case
+  if (n == 1) {
+    cout << 1 << " ";
+    return;
+  }
+  
+  // Processing
+  cout << n << " ";
+  
+  // Recursive call
+  counting(n - 1);
+}
+
+int main() {
+  int n;
+  cout << "Enter the number: ";
+  cin >> n;
+  counting(n);
+  
+  return 0;
+}
+
+
+
+
